03ee3848e1605f5c37101a05c0e8110d777fff46
HolbanHD <holbanhd@gmail.com>
2025-01-02 19:04:58 +0200

    add player fps movement
    הוספתי לחשקן את הלוגיה הבסיסית לתנועה... קפיצה... דראג.
    
    מצלמה נמצאת כרגע בתוך השחקן להתנסות ראשונה
    
    בהמשך: רוצה לעשות גוף שלישי

--- TBD_GameName/Assets/scripts/MouseMovement.cs
+++ TBD_GameName/Assets/scripts/MouseMovement.cs
@@ -1,38 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 /// <summary>
-/// 
+/// Located on the camera itself and makes it move according to the mouse
 /// </summary>
 
 public class NouseMovement : MonoBehaviour
 {
+    //__________________________________________________________________________ Variables
+    [SerializeField] private float mouseSensitivity = 400f;
 
-    [SerializeField] private float mouseSensitivity = 800f;
+    float xMouseSensitivity;
+    float yMouseSensitivity;
 
-    float xMousePos;
-    float yMousePos;
+    float xMouseRotation;
+    float yMouseRotation;
 
-    float xRotation;
-    float yRotation;
+    Transform playerOrientation;
 
-
+    //__________________________________________________________________________ Run
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        playerOrientation = GameObject.Find("PlayerOrientation").transform;
     }
 
     void Update()
     {
-        xMousePos = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        yMousePos = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        MoveCamera();
+    }
 
-        xRotation -= yMousePos;
-        yRotation += xMousePos;
+    //__________________________________________________________________________ Mathods
+    private void MoveCamera()
+    {
+        xMouseSensitivity = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        yMouseSensitivity = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xMouseRotation -= yMouseSensitivity;
+        yMouseRotation += xMouseSensitivity;
 
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        //Limiting movement on the Y axis
+        xMouseRotation = Mathf.Clamp(xMouseRotation, -90, 90);
 
-        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
+        transform.rotation = Quaternion.Euler(xMouseRotation, yMouseRotation, 0);
+        playerOrientation.rotation = Quaternion.Euler(0, yMouseRotation, 0);
     }
 }
+
--- TBD_GameName/Assets/scripts/PlayerMovement.cs
+++ TBD_GameName/Assets/scripts/PlayerMovement.cs
@@ -9,7 +9,127 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     //__________________________________________________________________________ Variables
+    Rigidbody rb;
+
+    //movement
+    [SerializeField] float moveSpeed;
+    [SerializeField] float groundDrag;
+
+    //jump
+    [SerializeField] float jumpForce;
+    [SerializeField] float jumpCooldown;
+    [SerializeField] float airMultiplier;
+    bool readyToJump;
+
+    //[SerializeField] float walkSpeed;
+    //[SerializeField] float sprintSpeed;
+
+    //public KeyCode jumpKey = KeyCode.Space;
+
+    [SerializeField] float playerHeight;
+    public LayerMask whatIsGround;
+    [SerializeField] bool grounded;
+
+    public Transform playerOrientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+
+    //__________________________________________________________________________ Run
+    private void Start()
+    {
+        playerOrientation = GameObject.Find("PlayerOrientation").transform;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        readyToJump = true;
+    }
+
+    private void Update()
+    {
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        
+        PlayerInput();
+        SpeedControl();
+
+        // handle drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+
+    //__________________________________________________________________________ Mathods
+    private void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // when to jump
+        if (Input.GetButton("Jump") && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = playerOrientation.forward * verticalInput + playerOrientation.right * horizontalInput;
+
+        // on ground
+        if (grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // in air
+        else if (!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+}
+
+    /*//__________________________________________________________________________ Variables
     [SerializeField] CharacterController Controller;
 
     [SerializeField] float playerSpeed = 12f;
@@ -46,8 +166,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void IsPlayerOnGroundChack()
     {
-        onGround = Physics.CheckSphere(playerBoots.position , groundDistance, groundMask);
-        
+        onGround = Physics.CheckSphere(playerBoots.position, groundDistance, groundMask);
+
 
         if (onGround && velocity.y < 0)
         {
@@ -58,24 +178,22 @@ public class PlayerMovement : MonoBehaviour
     private void MovePlayer()
     {
 
-            moveDirX = Input.GetAxis("Horizontal");
-            moveDirZ = Input.GetAxis("Vertical");
+        moveDirX = Input.GetAxis("Horizontal");
+        moveDirZ = Input.GetAxis("Vertical");
 
-            Vector3 move = transform.right * moveDirX + transform.forward * moveDirZ;
+        Vector3 move = transform.right * moveDirX + transform.forward * moveDirZ;
 
-            Controller.Move(move * playerSpeed * Time.deltaTime);
+        Controller.Move(move * playerSpeed * Time.deltaTime);
 
     }
 
     private void JumpPlayer()
     {
 
-        if (Input.GetButton("Jump") && onGround == true) 
+        if (Input.GetButton("Jump") && onGround == true)
         {
             velocity.y = Mathf.Sqrt(playerJumpHeight * -2f * playerGravity);
         }
         velocity.y += playerGravity * Time.deltaTime;
         Controller.Move(velocity * Time.deltaTime);
-    }
-
-}
+    }*/
