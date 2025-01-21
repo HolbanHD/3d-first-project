1caaa5c964af19825e12f6541c1a1b1d99d47e28
tomer-mend <153426720+tomer-mend@users.noreply.github.com>
2025-01-22 00:53:13 +0200

    Added IInteractable interface, abstract NPC class

--- TBD_GameName/Assets/Scripts/NPCs/NPC.cs
+++ TBD_GameName/Assets/Scripts/NPCs/NPC.cs
@@ -4,6 +4,27 @@ namespace NPC
 {
     public abstract class NPC : MonoBehaviour
     {
+        [SerializeField] protected NPCData data;
 
+        protected float currentHealth;
+
+        protected virtual void Awake()
+        {
+            currentHealth = data.MaxHealth;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            currentHealth -= damage;
+            if (currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+
+        protected virtual void Die()
+        {
+            //add death logic
+        }
     }
 }
