3645ca0e0a17406e0906adf589bfa14dd83103e4
tomer-mend <153426720+tomer-mend@users.noreply.github.com>
2025-01-22 02:57:31 +0200

    Added model, finished abstract NPC class

---
+++ TBD_GameName/Assets/Scripts/Interfaces/IDamagable.cs
@@ -0,0 +1,9 @@
+
+namespace Interfaces
+{
+    public interface IDamagable
+    {
+        void TakeDamage(float damage);
+        void Die();
+    }
+}
--- TBD_GameName/Assets/Scripts/NPCs/NPC.cs
+++ TBD_GameName/Assets/Scripts/NPCs/NPC.cs
@@ -1,19 +1,34 @@
+using Interfaces;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace NPC
 {
-    public abstract class NPC : MonoBehaviour
+    [RequireComponent(typeof(NavMeshAgent))]
+    public abstract class NPC : MonoBehaviour, IDamagable
     {
         [SerializeField] protected NPCData data;
 
         protected float currentHealth;
+        protected NavMeshAgent agent;
 
         protected virtual void Awake()
         {
+            Init();
+        }
+
+        protected virtual void Init()
+        {
+            agent = GetComponent<NavMeshAgent>();
             currentHealth = data.MaxHealth;
         }
 
-        public void TakeDamage(float damage)
+        public virtual void MoveTo(Vector3 position)
+        {
+            agent.SetDestination(position);
+        }
+
+        public virtual void TakeDamage(float damage)
         {
             currentHealth -= damage;
             if (currentHealth <= 0)
@@ -22,7 +37,7 @@ namespace NPC
             }
         }
 
-        protected virtual void Die()
+        public virtual void Die()
         {
             //add death logic
         }
